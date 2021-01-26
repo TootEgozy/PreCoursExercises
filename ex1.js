778fa79c475867c9a34ec7ddc0a3f903d1428c41
Toot Egozy <strawberevil@gmail.com>
2021-01-26 18:09:46 +0200

    Version2

--- ex1.js
+++ ex1.js
@@ -1,5 +1,6 @@
 var readlineSync = require('readline-sync');
  
 // Get the user's name
+// Hello world!
 var userName = readlineSync.question('Please enter your name: ');
 console.log('Hello ' + userName + '!');
